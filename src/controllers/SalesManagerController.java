019c1a417d056c3b758b1c5fdd440b3d3bfb27a5
yongliang <yongliang327@gmail.com>
2025-05-23 17:47:50 +0800

    restructure

--- src/controllers/SalesManagerController.java
+++ src/controllers/SalesManagerController.java
@@ -1,27 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controllers;
 
-import helpers.DailySalesTableHelper;
-import helpers.ItemTableHelper;
-import helpers.PurchaseOrderTableHelper;
-import helpers.SupplierTableHelper;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.List;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.JTable;
-import javax.swing.SpinnerNumberModel;
-import javax.swing.table.DefaultTableModel;
+import helpers.*;
 import models.Item;
 import models.Sales;
 import models.users.SalesManager;
@@ -31,26 +10,24 @@ import services.DateTimeService;
 import services.IdGenerator;
 import views.SalesManagerDashboard;
 
-/**
- *
- * @author Chan Yong Liang
- */
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
 public class SalesManagerController extends BaseController {
 
     private SalesManagerDashboard dashboard;
     private IdGenerator idGenerator = new IdGenerator();
 
-    // item tables
+    // Tables
     private JTable itemOnSaleTable;
     private JTable itemNotOnSaleTable;
-
-    //  Daily Sales
     private JTable dailySalesTable;
-
-    // Purchase Order
     private JTable purchaseOrderTable;
-
-    //Suppliers
+    private JTable historicalRequisitionTable;
+    private JTable pendingRequisitionTable;
     private JTable supplierTable;
 
     public SalesManagerController(SalesManager salesManagerUser) {
@@ -65,95 +42,83 @@ public class SalesManagerController extends BaseController {
 
     @Override
     protected void loadInitialData() {
-        populateTables();
-        setSaleTotal();
-
+        populateAllTables();
+        updateTotalSaleAmount();
     }
 
     @Override
     protected void setupCustomListeners() {
-        addSale();
+        setupAddSaleListener();
     }
 
-    private void addSale() {
-        JButton addSalesButton = dashboard.getAddSalesButton();
-
-        addSalesButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ItemRepository itemRepo = new ItemRepository();
-                List<Item> itemOnSaleList = itemRepo.getAll()
-                        .stream()
-                        .filter(item -> item.getStatus() == Item.Status.onSale)
-                        .toList();
-
-                JComboBox<Item> comboBox = new JComboBox<>(itemOnSaleList.toArray(new Item[0]));
-
-                JSpinner quantitySpinner = new JSpinner(new SpinnerNumberModel(1, 1, 1000, 1));
-
-                JPanel panel = addSaleDialogPanel(comboBox, quantitySpinner);
-
-                int result = JOptionPane.showConfirmDialog(null, panel, "Add Sale", JOptionPane.OK_CANCEL_OPTION);
-
-                if (result == JOptionPane.OK_OPTION) {
-                    Item selectedItem = (Item) comboBox.getSelectedItem();
-                    int quantity = (Integer) quantitySpinner.getValue();
-
-                    String generatedSaleId = idGenerator.generateNewId(Sales.class);
-                    double totalAmount = selectedItem.getSellPrice() * quantity;
-
-                    String currentDate = DateTimeService.getCurrentDate();
-                    String currentTime = DateTimeService.getCurrentTime();
-
-                    Sales newSale = new Sales(generatedSaleId, selectedItem.getItemId(), quantity, currentDate, currentTime, totalAmount);
-
-                    SalesRepository salesRepo = new SalesRepository();
-                    salesRepo.save(newSale);
-
-                    JOptionPane.showMessageDialog(null, "Sale recorded successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
-
-                    dailySalesTable = dashboard.getSalesTable();
-                    DefaultTableModel dailySaleModel = (DefaultTableModel) dailySalesTable.getModel();
-                    DailySalesTableHelper.populateTodaySales(dailySaleModel);
-                    setSaleTotal();
-
-                }
-
-            }
-        });
+    private void populateAllTables() {
+        populateItemTables();
+        populateSalesTable();
+        populatePurchaseOrderTable();
+        populateRequisitionTables();
+        populateSupplierTable();
     }
 
-    private void populateTables() {
+    private void populateItemTables() {
         itemOnSaleTable = dashboard.getItemOnSaleTable();
         itemNotOnSaleTable = dashboard.getItemNotOnSaleTable();
-
-        // item on sale table population
-        DefaultTableModel itemOnSaleTableModel = (DefaultTableModel) itemOnSaleTable.getModel();
-        ItemTableHelper.populateItemOnSale(itemOnSaleTableModel);
-
-        // item not on sale table population
-        DefaultTableModel itemNotOnSaleTableModel = (DefaultTableModel) itemNotOnSaleTable.getModel();
-        ItemTableHelper.populateItemNotOnSale(itemNotOnSaleTableModel);
-
-        dailySalesTable = dashboard.getSalesTable();
-        DefaultTableModel dailySaleModel = (DefaultTableModel) dailySalesTable.getModel();
-        DailySalesTableHelper.populateTodaySales(dailySaleModel);
-
-        purchaseOrderTable = dashboard.getOrderTable();
-        DefaultTableModel orderModel = (DefaultTableModel) purchaseOrderTable.getModel();
-        PurchaseOrderTableHelper.populatePurchaseOrder(orderModel);
-
-        // Supplier
-        supplierTable = dashboard.getSupplierTable();
-        DefaultTableModel supplierModel = (DefaultTableModel) supplierTable.getModel();
-        SupplierTableHelper.populateSupplier(supplierModel);
+        ItemTableHelper.populateItemOnSale((DefaultTableModel) itemOnSaleTable.getModel());
+        ItemTableHelper.populateItemNotOnSale((DefaultTableModel) itemNotOnSaleTable.getModel());
     }
 
-    private void setSaleTotal() {
+    private void populateSalesTable() {
+        dailySalesTable = dashboard.getSalesTable();
+        DailySalesTableHelper.populateTodaySales((DefaultTableModel) dailySalesTable.getModel());
+    }
+
+    private void populatePurchaseOrderTable() {
+        purchaseOrderTable = dashboard.getOrderTable();
+        PurchaseOrderTableHelper.populatePurchaseOrder((DefaultTableModel) purchaseOrderTable.getModel());
+    }
+
+    private void populateRequisitionTables() {
+        historicalRequisitionTable = dashboard.getHistoricalRequisitionTable();
+        pendingRequisitionTable = dashboard.getPendingRequisitionTable();
+        DefaultTableModel pendingModel = (DefaultTableModel) pendingRequisitionTable.getModel();
+        DefaultTableModel historicalModel = (DefaultTableModel) historicalRequisitionTable.getModel();
+        PurchaseRequisitionsTableHelper.populateAllRequisitions(pendingModel, historicalModel);
+    }
+
+    private void populateSupplierTable() {
+        supplierTable = dashboard.getSupplierTable();
+        SupplierTableHelper.populateSupplier((DefaultTableModel) supplierTable.getModel());
+    }
+
+    private void updateTotalSaleAmount() {
         JLabel totalAmountText = dashboard.getTotalAmount();
         totalAmountText.setText(DailySalesTableHelper.calculateColumnTotal(dailySalesTable, 6));
     }
 
-    private JPanel addSaleDialogPanel(JComboBox<Item> comboBox, JSpinner spinner) {
+    private void setupAddSaleListener() {
+        JButton addSalesButton = dashboard.getAddSalesButton();
+        addSalesButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                showAddSaleDialog();
+            }
+        });
+    }
+
+    private void showAddSaleDialog() {
+        List<Item> itemOnSaleList = new ItemRepository().getAll().stream()
+                .filter(item -> item.getStatus() == Item.Status.onSale)
+                .toList();
+
+        JComboBox<Item> comboBox = new JComboBox<>(itemOnSaleList.toArray(new Item[0]));
+        JSpinner quantitySpinner = new JSpinner(new SpinnerNumberModel(1, 1, 1000, 1));
+        JPanel panel = createAddSalePanel(comboBox, quantitySpinner);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "Add Sale", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            processAddSale((Item) comboBox.getSelectedItem(), (Integer) quantitySpinner.getValue());
+        }
+    }
+
+    private JPanel createAddSalePanel(JComboBox<Item> comboBox, JSpinner spinner) {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.add(new JLabel("Select Item:"));
@@ -163,4 +128,18 @@ public class SalesManagerController extends BaseController {
         return panel;
     }
 
+    private void processAddSale(Item selectedItem, int quantity) {
+        String generatedSaleId = idGenerator.generateNewId(Sales.class);
+        double totalAmount = selectedItem.getSellPrice() * quantity;
+        String currentDate = DateTimeService.getCurrentDate();
+        String currentTime = DateTimeService.getCurrentTime();
+
+        Sales newSale = new Sales(generatedSaleId, selectedItem.getItemId(), quantity, currentDate, currentTime, totalAmount);
+        new SalesRepository().save(newSale);
+
+        JOptionPane.showMessageDialog(null, "Sale recorded successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+
+        DailySalesTableHelper.populateTodaySales((DefaultTableModel) dailySalesTable.getModel());
+        updateTotalSaleAmount();
+    }
 }
